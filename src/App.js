9ab8f2de7a368edeaa028dc728e8a4553a475997
ambika2404 <ambika.kumari@hotmail.com>
2022-09-27 12:01:20 -0700

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,13 +5,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather App</h1>
-      <footer>
-        This project was coded by Ambika Kumari and is
-        <a href="https://github.com/ambika2404/weather-react" target="_blank">
-          open-source on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather App</h1>
+        <footer>
+          This project was coded by Ambika Kumari and is {""}
+          <a
+            href="https://github.com/ambika2404/weather-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-source on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
