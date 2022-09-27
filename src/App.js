0b0f4ef9d794d001e7de99588be24d00562104d2
ambika2404 <ambika.kumari@hotmail.com>
2022-09-26 20:28:54 -0700

    Added Github repository URL

--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <h1> Weather App</h1>
+      <footer>
+        This project was coded by Ambika Kumari and is
+        <a href="https://github.com/ambika2404/weather-react" target="_blank">
+          open-source on Github
+        </a>
+      </footer>
     </div>
   );
 }
