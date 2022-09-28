9a2df80f6952d4c5b772a601e8f8a9d808cb77d1
ambika2404 <ambika.kumari@hotmail.com>
2022-09-27 17:55:39 -0700

    Added API Integration

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="Fremont" />
         <footer>
           This project was coded by Ambika Kumari and is {""}
           <a
--- src/Weather.js
+++ src/Weather.js
@@ -1,44 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+
 import "./Weather.css";
-export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+      date: "Tuesday 05:51pm",
+      icon: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city"
+                className="form-control"
+              />
+            </div>
+            <div className="col-3">
+              <input type="submit" value="search" className="btn btn-primary" />
+            </div>
+          </div>
+        </form>
+        <h3>{weatherData.city}</h3>
+        <ul>
+          <li>{weatherData.date}</li>
+          <li>{weatherData.description}</li>
+        </ul>
         <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a city"
-              className="form-control"
-            />
+          <div className="col-6">
+            <img src={weatherData.icon} alt={weatherData.description} />
+            <span className="temperature">
+              {" "}
+              {Math.round(weatherData.temperature)}
+            </span>
+            <span className="unit">°C</span>
           </div>
-          <div className="col-3">
-            <input type="submit" value="search" className="btn btn-primary" />
+          <div className="col-6">
+            <ul>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li> Wind: {weatherData.wind}km/hr</li>
+            </ul>
           </div>
-        </div>
-      </form>
-      <h3>Fremont,USA</h3>
-      <ul>
-        <li>Tuesday, 12:10pm</li>
-        <li>Mostly Cloudy</li>
-      </ul>
-      <div className="row">
-        <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
-            alt="sunny"
-          />
-          <span className="temperature"> 18</span>
-          <span className="unit">°C</span>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Precipitation: 1%</li>
-            <li>Humidity: 64%</li>
-            <li> Wind: 11 km/h</li>
-          </ul>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "931dc49fba96c8c62bbb87a9dc503a65";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading";
+  }
 }
