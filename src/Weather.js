e78777428d7a32feb8fb95f8bc040bd937a17fd4
ambika2404 <ambika.kumari@hotmail.com>
2022-10-10 11:36:05 -0700

    Used correct icon URL

--- src/Weather.js
+++ src/Weather.js
@@ -15,7 +15,7 @@ export default function Weather(props) {
       wind: response.data.wind.speed,
       city: response.data.name,
       date: new Date(response.data.dt * 1000),
-      icon: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       description: response.data.weather[0].description,
       humidity: response.data.main.humidity,
     });
