85d4cfe179b5af21eabaed328ee5ea7eb9cd39f0
ambika2404 <ambika.kumari@hotmail.com>
2022-10-18 11:46:27 -0700

    Updated WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -16,6 +16,14 @@ export default function WeatherForecast(props) {
     setForecast(response.data.daily);
     setLoaded(true);
   }
+  function load() {
+    let apiKey = "34ae1065362d42545661451bda2b8a1f";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+  }
 
   if (loaded) {
     console.log(forecast);
@@ -37,12 +45,8 @@ export default function WeatherForecast(props) {
       </div>
     );
   } else {
-    let apiKey = "34ae1065362d42545661451bda2b8a1f";
-    let longitude = props.coordinates.lon;
-    let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    load();
 
-    axios.get(apiUrl).then(handleResponse);
     return null;
   }
 }
